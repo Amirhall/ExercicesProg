098f303157e6ee8927011c47c75d942429d7f8b9
Amir Hal Houssine <2284873@cegepmontpetit.ca>
2024-02-28 12:43:37 -0500

    formatif

--- Application Mobile/Cours 5 Acces reseau/Formatif/app/src/main/java/com/example/formatif/MainActivity.java
+++ Application Mobile/Cours 5 Acces reseau/Formatif/app/src/main/java/com/example/formatif/MainActivity.java
@@ -1,10 +1,10 @@
-package org.amir.retrofitcomplexe;
+package com.example.formatif;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class  MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

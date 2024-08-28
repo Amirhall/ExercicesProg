bd4ad6ef58cf0a2b662e7b5e2034e218f6374f01
Amir Hal Houssine <2284873@cegepmontpetit.ca>
2024-08-28 16:27:04 -0400

    application avancé cours 1

---
+++ TapeLeLapin/android/app/src/main/kotlin/com/example/tapelelapin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tapelelapin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ drapeauitalie/android/app/src/main/kotlin/com/example/drapeauitalie/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.drapeauitalie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()

980aabb413e3bc9046e655fa4e4a17820cb74a59
Amir Hal Houssine <2284873@cegepmontpetit.ca>
2024-10-02 15:17:59 -0400

    sadas
    dsadsadsa

---
+++ imagepicker/lib/form.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:imagepicker/main.dart';
+class form extends StatefulWidget {
+  const form({super.key});
+  @override
+  State<form> createState() => _formState();
+}
+
+class _formState extends State<form> {
+  String imagePath = "";
+  void getImage() async{
+    ImagePicker picker = ImagePicker();
+    XFile? pickedImage = await picker.pickImage(source: ImageSource.gallery);
+    imagePath = pickedImage!.path;
+    setState(() {});
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: Image.file(File(imagePath)).image,
+            // if(imagePath == "") { Image.asset(imagePath).image } else {Image.asset(imagePath).image}
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Prénom"),
+            TextField(),
+            Text("Nom"),
+            TextField(),
+            Text("Mot de passe"),
+            TextField(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const MyApp(),
+                        ),
+                      );
+                    }, child: Text("Retour menu")),
+                ElevatedButton(
+                    onPressed: () {
+                      getImage();
+                      setState(() {});
+                    }, child: Text("Changer background")),
+              ],
+            )
+          ],
+        ),
+      )
+
+    );
+  }
+}

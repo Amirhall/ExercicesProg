69e522573a2241ec5a8230c8d1bb23531df9e620
Amir Hal Houssine <2284873@cegepmontpetit.ca>
2024-10-02 15:54:52 -0400

    sadsa
    dasdasdasdasd

--- imagepicker/lib/envoi_images.dart
+++ imagepicker/lib/envoi_images.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:imagepicker/main.dart';
@@ -11,53 +12,57 @@ class envoie_images extends StatefulWidget {
 
 class _envoie_imagesState extends State<envoie_images> {
   String imagePath = "";
+  XFile? pickedImage;
+  String ImageURL = "";
+
   void getImage() async{
     ImagePicker picker = ImagePicker();
-    XFile? pickedImage = await picker.pickImage(source: ImageSource.gallery);
+    pickedImage = await picker.pickImage(source: ImageSource.gallery);
     imagePath = pickedImage!.path;
+    sendImage();
+  }
+
+  void sendImage() async{
+    FormData formData = FormData.fromMap({"file": await MultipartFile.fromFile(imagePath, filename: pickedImage!.name)});
+    Dio dio = Dio();
+    var response = await dio.post("http://10.0.2.2:8080/singleFile", data:formData);
+    String id = response.data as String;
+    ImageURL = "http://10.0.2.2:8080/singleFile" + id;
     setState(() {});
+
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: Image.file(File(imagePath)).image,
-              // if(imagePath == "") { Image.asset(imagePath).image } else {Image.asset(imagePath).image}
-              fit: BoxFit.cover,
+        body: SizedBox.expand(child:
+        Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: Image.network(ImageURL).image,
+                // if(imagePath == "") { Image.asset(imagePath).image } else {Image.asset(imagePath).image}
+                fit: BoxFit.fitWidth,
+              ),
             ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Prénom"),
-              TextField(),
-              Text("Nom"),
-              TextField(),
-              Text("Mot de passe"),
-              TextField(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const MyApp(),
-                          ),
-                        );
-                      }, child: Text("Retour menu")),
-                  ElevatedButton(
-                      onPressed: () {
-                        getImage();
-                        setState(() {});
-                      }, child: Text("Changer background")),
-                ],
-              )
-            ],
-          ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const MyApp(),
+                        ),
+                      );
+                    }, child: Text("Retour menu")),
+                ElevatedButton(
+                    onPressed: () {
+                      getImage();
+                      setState(() {});
+                    }, child: Text("Envoyer une image")),
+              ],
+            ),
+        ),
         )
 
     );
--- imagepicker/lib/form.dart
+++ imagepicker/lib/form.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:imagepicker/main.dart';
